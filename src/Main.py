01ab4032eec747a0bfddca46953f4aaa3f06cb72
Murto <murto@airmail.cc>
2018-11-17 17:47:01 +0000

    Add two simple commands to test bot capailities

--- src/Main.py
+++ src/Main.py
@@ -9,6 +9,24 @@ prefix = "!"
 description = "Test description please ignore"
 bot = commands.Bot(command_prefix=prefix, description=description)
 
+
+@bot.event
+async def on_ready():
+  print("Bot ready")
+
+
+@bot.command()
+async def ping():
+  await bot.say("pong")
+
+
+@bot.command()
+async def github():
+  githubFile = open("data/github.txt")
+  url = githubFile.read().strip()
+  await bot.say("Github: %s" % url)
+
+
 tokenFile = open("token/token.txt")
 if (not tokenFile):
   print("Token could not be read, exiting...")
