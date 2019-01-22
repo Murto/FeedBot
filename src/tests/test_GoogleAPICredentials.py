f691761b2ff6a86580d7477b2cdf5c20cf170181
Murto <murto@airmail.cc>
2019-01-22 19:24:43 +0000

    Add Google API credentials test

---
+++ src/tests/test_GoogleAPICredentials.py
@@ -0,0 +1,24 @@
+from GoogleAPICredentials import GoogleAPICredentials
+import unittest
+
+
+class GoogleAPICredentialsTest(unittest.TestCase):
+
+  CREDENTIALS_PATH = "test/path/credentials.json"
+  TOKEN_PATH = "test/path/token.json"
+
+  def test_get_credentials_path(self):
+    creds = GoogleAPICredentials(self.CREDENTIALS_PATH, self.TOKEN_PATH)
+    self.assertEqual(creds.get_credentials_path(), self.CREDENTIALS_PATH, msg = "Incorrect credentials path")
+
+  def test_get_token_path(self):
+    creds = GoogleAPICredentials(self.CREDENTIALS_PATH, self.TOKEN_PATH)
+    self.assertEqual(creds.get_token_path(), self.TOKEN_PATH, msg = "Incorrect token path")
+
+
+def main():
+  unittest.main()
+
+
+if (__name__ == "__main__"):
+  main()
